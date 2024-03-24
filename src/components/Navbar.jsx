6ceda3d4f4a86d298fb1e36afe8fb921f61c65fe
rmadayag <rcm1119@students.uc-bcf.eu.ph>
2024-03-24 18:41:56 +0800

    Your commit message here

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from '/logo.svg' ;
+import logo from '/full-logo.svg' ;
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
