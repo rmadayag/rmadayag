4286caa1c2668fe430cfff2f3c04ecfc993db14b
Rheynalyn Madayag <rmady@Rheynalyns-MacBook-Air.local>
2025-05-18 23:32:06 +0800

    Your commit message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,10 @@ import { GoArrowUpRight } from "react-icons/go";
 const Footer = () => {
   const socialLinks = [
     { label: "LINKEDIN", href: "https://www.linkedin.com/in/rheynalyn-madayag-998100242/" },
-    { label: "INSTAGRAM", href: "https://www.instagram.com/" },
-    { label: "FACEBOOK", href: "https://www.facebook.com/" },
+    { label: "FIGMA", href: "https://www.figma.com/design/FhhsMvcQyprSZXCErcCbbX/Figma-Portfolio?node-id=1-11&t=RvemA4lO480GHeaM-1" },
+    { label: "BEHANCE", href: "https://www.behance.net/rheynalmadayag" },
     { label: "GITHUB", href: "https://github.com/rmadayag" },
+
   ];
 
   return (
