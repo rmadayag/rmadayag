8fa85fcce2f3c5ed7c2338dda9cfaefcbe0e8b52
Rheynalyn Madayag <rmady@Rheynalyns-MacBook-Air.local>
2025-05-22 23:23:42 +0800

    Your commit message

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -19,6 +19,7 @@ import IMG12 from "../assets/images/img12.png";
 import IMG13 from "../assets/images/img13.png";
 import IMG14 from "../assets/images/img14.png";
 import IMG15 from "../assets/images/img15.png";
+import IMG16 from "../assets/images/img16.png";
 
 const allProjects = [
   { id: 1, title: "Brewing Brands", image: IMG1, description: "2025 • Web Development", url: "https://brewingbrandsph.com/" },
@@ -36,7 +37,7 @@ const allProjects = [
   { id: 11, title: "Digital Branding", image: IMG11, description: "2024 • Web Development", url: "https://digitalbrandingco.com.au/" },
   { id: 14, title: "NUMA", image: IMG14, description: "2022 • Web Design | Development", url: "https://numarecoverycenters.com/" },
   { id: 15, title: "TribeTXT", image: IMG15, description: "2022 • Web Design | Development", url: "https://business.tribetxt.com/" },
-  { id: 16, title: "Emergency Plumbers", image: IMG15, description: "2025 • Web Design | Development", url: "https://emergencyplumbersmelbourne.au/" },
+  { id: 16, title: "Emergency Plumbers", image: IMG16, description: "2025 • Web Design | Development", url: "https://emergencyplumbersmelbourne.au/" },
 ];
 
 const Projects = () => {
